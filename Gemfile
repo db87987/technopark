e920e96e2fb5d038848d73a3f97c1cee2e7ebe57
Viacheslav Bobrov <babrovka@gmail.com>
2013-03-02 19:16:28 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'sqlite3'
+
+gem "paperclip", "~> 3.0"
+gem "friendly_id", "~> 4.0.9"
+gem 'russian', '~> 0.6.0'
+gem 'activeadmin'
+gem "meta_search",    '>= 1.1.0.pre'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Technopark::Application.config.secret_token = '269d8c6a56567b3edb6ff3b13620d7ce2bab53853d8e4ae8884a5a511037567a8e5c2821521a4d36955d671b9a38a812f99f94758c64bae3e437539575c5cef2'
