cec52080662b0a84bc6b7f05a73d6c0e991b3092
Viacheslav Bobrov <babrovka@gmail.com>
2013-03-05 18:31:05 +0400

    static files del migration

--- db/migrate/20130305123531_add_files_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddFilesToArticles < ActiveRecord::Migration
-  def change
-    add_attachment :articles, :file1
-    add_attachment :articles, :file2
-    add_attachment :articles, :file3
-  end
-end
