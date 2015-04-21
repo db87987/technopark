9d5238d3c93c52452e676ec47d4add5d80ae9a84
Vyacheslav Bobrov <babrovka@gmail.com>
2015-04-21 15:20:39 +0300

    deploy to locum

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,53 +1,108 @@
-require 'rvm/capistrano'
+# По умолчанию для дистрибуции проектов используется Bundler.
+# Эта строка включает автоматическое обновление и установку
+# недостающих gems, указанных в вашем Gemfile.
+#
+## !!! Не забудьте добавить
+# gem 'capistrano'
+# gem 'unicorn'
+#
+# в ваш Gemfile.
+#
+# Если вы используете другую систему управления зависимостями,
+# закомментируйте эту строку.
 require 'bundler/capistrano'
 
-server "194.190.225.173", :web, :app, :db, primary: true
+## Чтобы не хранить database.yml в системе контроля версий, поместите
+## dayabase.yml в shared-каталог проекта на сервере и раскомментируйте
+## следующие строки.
 
-set :user, "tpark"
-set :application, "technopark"
-set :deploy_to, "/home/tpark/#{application}"
-set :deploy_via, :remote_cache
-set :use_sudo, false
+before "deploy:assets:precompile", :copy_database_config
+task :copy_database_config, roles => :app do
+  db_config = "#{shared_path}/database.yml"
+  run "cp #{db_config} #{release_path}/config/database.yml"
+end
 
-set :scm, "git"
-set :repository, "git@github.com:babrovka/technopark.git"
-set :branch, "master"
+# В rails 3 по умолчанию включена функция assets pipelining,
+# которая позволяет значительно уменьшить размер статических
+# файлов css и js.
+# Эта строка автоматически запускает процесс подготовки
+# сжатых файлов статики при деплое.
+# Если вы не используете assets pipelining в своем проекте,
+# или у вас старая версия rails, закомментируйте эту строку.
+load 'deploy/assets'
 
-default_run_options[:pty] = true
+# Для удобства работы мы рекомендуем вам настроить авторизацию
+# SSH по ключу. При работе capistrano будет использоваться
+# ssh-agent, который предоставляет возможность пробрасывать
+# авторизацию на другие хосты.
+# Если вы не используете авторизацию SSH по ключам И ssh-agent,
+# закомментируйте эту опцию.
 ssh_options[:forward_agent] = true
 
+# Имя вашего проекта в панели управления.
+# Не меняйте это значение без необходимости, оно используется дальше.
+set :application,     "technopark"
 
+# Сервер размещения проекта.
+set :deploy_server,   "hydrogen.locum.ru"
+
+# Не включать в поставку разработческие инструменты и пакеты тестирования.
+set :bundle_without,  [:development, :test]
+
+set :user,            "hosting_ssw78"
+set :login,           "ssw78"
+set :use_sudo,        false
+set :deploy_to,       "/home/#{user}/projects/#{application}"
+set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
+set :unicorn_pid,     "/var/run/unicorn/#{user}/#{application}.#{login}.pid"
+set :bundle_dir,      File.join(fetch(:shared_path), 'gems')
+role :web,            deploy_server
+role :app,            deploy_server
+role :db,             deploy_server, :primary => true
+
+# Следующие строки необходимы, т.к. ваш проект использует rvm.
+set :rvm_ruby_string, "2.2.0"
+set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake"
+set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
+
+# Настройка системы контроля версий и репозитария,
+# по умолчанию - git, если используется иная система версий,
+# нужно изменить значение scm.
+set :scm,             :git
+
+# Предполагается, что вы размещаете репозиторий Git в вашем
+# домашнем каталоге в подкаталоге git/<имя проекта>.git.
+# Подробнее о создании репозитория читайте в нашем блоге
+# http://locum.ru/blog/hosting/git-on-locum
+# set :repository,      "ssh://#{user}@#{deploy_server}/home/#{user}/git/#{application}.git"
+
+## Если ваш репозиторий в GitHub, используйте такую конфигурацию
+set :repository,    "git@github.com:babrovka/technopark.git"
+
+## --- Ниже этого места ничего менять скорее всего не нужно ---
+
+before 'deploy:finalize_update', 'set_current_release'
+task :set_current_release, :roles => :app do
+    set :current_release, latest_release
+end
+
+  set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+
+
+# - for unicorn - #
 namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
-end
-
-after "deploy:update_code", :copy_database_config
- task :copy_database_config, roles => :app do
-   db_config = "#{shared_path}/database.yml"
-   run "cp #{db_config} #{release_path}/config/database.yml"
-end
-
-namespace(:thin) do
-  task :stop do
-    run "thin stop -C /home/tpark/tpark.yml"
-   end
-  
-  task :start do
-    run "thin start -C /home/tpark/tpark.yml"
+  desc "Start application"
+  task :start, :roles => :app do
+    run unicorn_start_cmd
   end
 
-  task :restart do
-    run "thin restart -C C /home/tpark/tpark.yml"
+  desc "Stop application"
+  task :stop, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
 end
-
-after "deploy", "deploy:cleanup"
